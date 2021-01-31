3eba35c56b1e5ed054d4c553250d7e9ee4a6527e
Mariano Fuhr <marianofuhr@MacBook-Pro-de-Mariano.local>
2021-01-31 16:38:41 -0300

    agregado modulo de usuarios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
- /*  url: 'https://amps-backend-test.herokuapp.com', */
-  url: 'http://localhost:8080'
+  url: 'https://amps-backend-test.herokuapp.com',
+  /* url: 'http://localhost:8080' */
 };
