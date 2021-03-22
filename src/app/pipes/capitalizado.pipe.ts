19533c0535c561583ab9281619b0f9e568d7846d
Mariano Fuhr <mfuhr91@gmail.com>
2021-03-22 20:59:04 -0300

    cambios para deply en heroku

--- src/app/pipes/capitalizado.pipe.ts
+++ src/app/pipes/capitalizado.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CapitalizadoPipe implements PipeTransform {
 
-  transform(value: string ): string {
+  transform(value: string ): string | null {
    
     value = value.charAt(0).toUpperCase() + value.substr(1);
     
